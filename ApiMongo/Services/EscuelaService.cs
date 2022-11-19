95d56cdc76b0d04f7b8c3476a99f84c497b136a6
Armando <oavg30gmail.com>
2022-11-19 11:19:41 -0600

    actividad2

---
+++ ApiMongo/Services/EscuelaService.cs
@@ -0,0 +1,37 @@
+﻿using ApiMongo.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiMongo.Services
+{
+    public class EscuelaService
+    {
+        private IMongoCollection<Alumnos> _alumnos;
+        public EscuelaService(IEscuelaSettings settings)
+        {
+            var cliente = new MongoClient(settings.Server);
+            var database = cliente.GetDatabase(settings.Database);
+            _alumnos = database.GetCollection<Alumnos>(settings.Collection);
+
+        }
+        public List<Alumnos> Get()
+        {
+            return _alumnos.Find(d => true).ToList();
+        }
+        public Alumnos Create(Alumnos alumnos)
+        {
+            _alumnos.InsertOne(alumnos);
+            return alumnos;
+        }
+        public void Update(string id, Alumnos alumnos)
+        {
+            _alumnos.ReplaceOne(alumnos => alumnos.Id == id, alumnos);
+
+        }
+        public void Delete(string id)
+        {
+            _alumnos.DeleteOne(d => d.Id == id);
+        }
+    }
+}
